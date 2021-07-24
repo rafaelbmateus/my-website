14d173046cd02454397b468908338323ba6aefdf
Rafael Mateus <rafael.mateus@neoway.com.br>
2021-07-24 10:16:28 -0300

    fix: change docusaurus configs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,13 +5,14 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 module.exports = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://rafaelbmateus.github.io',
+  baseUrl: '/my-website/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'rafaelbmateus', // Usually your GitHub org/user name.
+  projectName: 'my-website', // Usually your repo name.
+  trailingSlash: false,
   themeConfig: {
     navbar: {
       title: 'My Site',
